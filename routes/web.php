3a7b9f6c6bd93b086e206ae255e72ffa7a932f8c
Jabes <jabesaribu@gmail.com>
2024-07-11 10:54:52 +0700

    UI - auth page & dashboard page ready

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     // return view('welcome');
-    return view('pages.blank-page', ['type_menu' => '']);
+    // return view('pages.blank-page', );
+    return view('pages.apps.dashboard-siakad', ['type_menu' => '']);
 });
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('pages.auth.auth-register',);
+})->name('register');
+
+Route::get('/forgot-password', function () {
+    return view('pages.auth.auth-forgot-password',);
+})->name('forgot-password');
+
+Route::get('/reset-password', function () {
+    return view('pages.auth.auth-reset-password',);
+})->name('reset-password');
+
